2cbd6eab00dd4e7784c41d8ad8f95f1d0f909af3
wendey <wendeyricardo@gmail.com>
2021-07-04 15:08:17 -0300

    criando, configurando, organizando e baixando as dependencias do projeto mobile

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        'module-resolver',
+        "module-resolver",
         {
-            alias: {
-              data: './src/data',
-              ui: './src/ui',
-              pages: './src/pages',
-              'assets': './assets',
-              'styles': './styles',
-            },
+          alias: {
+            data: "./src/data",
+            ui: "./src/ui",
+            pages: "./src/pages",
+            "@assets": "./assets",
+            "@styles": "./src/ui/styles",
+          },
         },
       ],
+      ["react-native-paper/babel"],
     ],
   };
 };
